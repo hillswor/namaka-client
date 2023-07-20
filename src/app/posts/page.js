a634120c4a5254899a49382698500c0105ffe5a9
Bruce <bruce.hillsworth@gmail.com>
2023-07-20 10:24:44 -0600

    Separate comments from posts in message board.

--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -10,8 +10,6 @@ export default function Posts() {
   const { posts } = useContext(PostsContext);
   const { setPost } = useContext(PostContext);
 
-  //   functions
-
   const toggleCommentForm = () => {
     setCommentForm(!commentForm);
   };
@@ -29,47 +27,50 @@ export default function Posts() {
       <section className="flex flex-col text-white">
         {posts &&
           posts.map((post) => (
-            <article
-              key={post.id}
-              className="bg-slate-500 rounded-md shadow-2xl p-8 mb-16"
-            >
-              <header className="mb-4">
-                <h2 className="text-xl font-bold">{post.title}</h2>
-              </header>
-              <p className="text-m">{post.content}</p>
-              <footer className="mt-4 text-xs">
-                <p className="text-gray-400">
-                  Posted by {post.user.email} on{" "}
-                  {new Date(post.created_at).toLocaleDateString()}
-                </p>
-              </footer>
-              <button
-                onClick={() => handleReplyClick(post)}
-                className="bg-red-500 px-4 py-2 rounded-lg hover:bg-red-400 mt-4 transition-all duration-200"
+            <>
+              <article
+                key={post.id}
+                className="bg-slate-500 rounded-md shadow-2xl p-8 mb-16"
               >
-                Reply
-              </button>
-              <header className="mb-2 mt-8">
-                <h2 className="text-l font-bold">Comments</h2>
-              </header>
+                <header className="mb-4">
+                  <h2 className="text-xl font-bold">{post.title}</h2>
+                </header>
+                <p className="text-m">{post.content}</p>
+                <footer className="mt-4 text-xs">
+                  <p className="text-gray-400">
+                    Posted by {post.user.email} on{" "}
+                    {new Date(post.created_at).toLocaleDateString()}
+                  </p>
+                </footer>
+                <button
+                  onClick={() => handleReplyClick(post)}
+                  className="bg-red-500 px-4 py-2 rounded-lg hover:bg-red-400 mt-4 transition-all duration-200"
+                >
+                  Reply
+                </button>
+              </article>
               {post.comments && post.comments.length > 0 && (
-                <div className="mt-4 bg-gray-200 px-2 py-4 rounded-md">
-                  <ul>
-                    {post.comments.map((comment) => (
-                      <li key={comment.id} className="text-sm">
-                        <p className="text-md text-blue-500">
-                          - {comment.content}
-                        </p>
-                        <p className="text-blue-400 text-xs mt-4 mb-8">
-                          Posted by {comment.user.email} on{" "}
-                          {new Date(comment.created_at).toLocaleDateString()}
-                        </p>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
+                <section className="mb-16">
+                  <header className="mb-2">
+                    <h2 className="text-l font-bold">Comments</h2>
+                  </header>
+                  {post.comments.map((comment) => (
+                    <article
+                      key={comment.id}
+                      className="bg-gray-100 px-4 py-2 rounded-md mb-4 shadow-md"
+                    >
+                      <p className="text-md text-blue-500">
+                        - {comment.content}
+                      </p>
+                      <footer className="text-blue-400 text-xs mt-2">
+                        Posted by {comment.user.email} on{" "}
+                        {new Date(comment.created_at).toLocaleDateString()}
+                      </footer>
+                    </article>
+                  ))}
+                </section>
               )}
-            </article>
+            </>
           ))}
       </section>
     </main>
